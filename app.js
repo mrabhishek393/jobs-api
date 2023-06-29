00afd0846927629feaaf2e9fe1fb369b9bc40e84
Abhishek Singh <abhisheksngh393@gmail.com>
2023-06-29 16:24:17 +0530

    Swagger added

--- app.js
+++ app.js
@@ -3,20 +3,32 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+//security packages
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerdocument = YAML.load("./swagger.yaml");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//database
 const connectdb = require("./db/connect");
+
+//router
 const authRouter = require("./routes/auth");
 const jobRouter = require("./routes/jobs");
+
+//authorization middleware
 const authorizeUser = require("./middleware/authentication");
 
 app.use(express.json());
+
 // extra packages
 app.set("trsut proxy", 1);
 app.use(
@@ -30,6 +42,11 @@ app.use(
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerdocument));
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authorizeUser, jobRouter);
